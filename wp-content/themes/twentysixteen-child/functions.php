70ecfbb12a2e9649a82042bf591171ec5fce8224
Alejandro López <oalopezp@unal.edu.co>
2018-05-13 17:31:13 -0500

    JS behavior changed. All JS in Header. No needed for id's to multiple CTA

--- wp-content/themes/twentysixteen-child/functions.php
+++ wp-content/themes/twentysixteen-child/functions.php
@@ -18,36 +18,49 @@ add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
 
 // CTA Countdown by Oscar Alejandro López Paz
 
-// cta countdown needs two basic functions on javascript to work correctly
+// cta countdown needs two basic functions on javascript to work correctly and two event listeners
 function basic_cta_functions()
 {
     echo '
     <script>
-    // General function to update every countdown by id and remaining time
-    function updateTime(cta_id) {
-        var cta = jQuery("#" + cta_id);
-        var now = new Date().getTime();
-        var remaining = new Date(cta.data("remaining")).getTime();
+    // General function to update every countdown remaining time
+    function updateTime(cta_class) {
+        jQuery(cta_class).each(function () {
+            var cta = jQuery(this);
+            var now = new Date().getTime();
+            var remaining = new Date(cta.data("remaining")).getTime();
 
-        // Set to 0 negative values
-        var difference = (remaining - now < 0) ? 0 : remaining - now;
+            // Set to 0 negative values
+            var difference = (remaining - now < 0) ? 0 : remaining - now;
 
-        var days = Math.floor(difference / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((difference % (1000 * 60)) / 1000);
+            var days = Math.floor(difference / (1000 * 60 * 60 * 24));
+            var hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((difference % (1000 * 60)) / 1000);
 
-        cta.find(".cta-days").html(days);
-        cta.find(".cta-hours").html(hours);
-        cta.find(".cta-min").html(minutes);
-        cta.find(".cta-sec").html(seconds);
+            cta.find(".cta-days").html(days);
+            cta.find(".cta-hours").html(hours);
+            cta.find(".cta-min").html(minutes);
+            cta.find(".cta-sec").html(seconds);
+        });
     }
 
     // Update the number of bets
-    function addBet(cta_id) {
-        var number_bets = jQuery("#" + cta_id).find(".cta-title-description b");
+    function addBet(cta) {
+        cta = cta.parent().parent().parent();
+        var number_bets = cta.find(".cta-title-description b");
         number_bets.html((parseInt(number_bets.html()) + 1).toString());
     }
+
+    jQuery(document).ready(function () {
+        setInterval(function () {
+            updateTime(".cta-countdown");
+        }, 1000);
+        
+        jQuery(".cta-button button").on("click", function () {
+        addBet(jQuery(this));
+    });
+    });
     </script>
     ';
 }
@@ -58,17 +71,15 @@ add_action('wp_head', 'basic_cta_functions');
 
 
 // CTA Countdown Shortcode
-//[cta title="Our NFL Pick: Patriots +5" remaining="2017/04/20 14:25:00" id="countdown1"]
-// id attribute is optional. Use this attribute if you need more than one CTA Countdown
+//[cta title="Our NFL Pick: Patriots +5" remaining="2017/04/20 14:25:00"]
 function cta_countdown_func($atts)
 {
     $attrs = shortcode_atts(array(
         'title' => 'Our NFL Pick: Vikings +3',
-        'remaining' => '2018/07/02 14:25:00 GMT-5',
-        'id' => 'countdown1'
+        'remaining' => '2018/07/02 14:25:00'
     ), $atts);
 
-    return '<div class="cta-countdown" data-remaining ="' . $attrs['remaining'] . '" id="' . $attrs['id'] . '">
+    return '<div class="cta-countdown" data-remaining ="' . $attrs['remaining'] . '">
         <div class="cta-body">
             <div class="cta-counter-row">
                 <div class="cta-counter">
@@ -116,19 +127,7 @@ function cta_countdown_func($atts)
 
             </div>
         </div>
-    </div>
-
-    <script>
-        jQuery("#' . $attrs['id'] . '").find(".cta-button button").on("click", function () {
-            addBet("' . $attrs['id'] . '");
-        });
-
-        jQuery(document) . ready(function () {
-            setInterval(function () {
-                updateTime("' . $attrs['id'] . '");
-                    }, 1000);
-        });
-    </script>';
+    </div>';
 
 }
 
